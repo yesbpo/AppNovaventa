9b0c8abb49e76a17fb96ab6680812b2d542fb3c0
SOPORTE 2023 <mesadeayuda@yesbpo.co>
2024-01-13 14:45:11 -0500

    firt commit

--- pages/users.js
+++ pages/users.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from '../components/Layout';
 import styled from 'styled-components';
 import { useSession, signIn } from 'next-auth/react';
