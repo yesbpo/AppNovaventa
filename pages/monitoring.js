75db21586ec6da04788e729298e4a4e8cbb2e6bd
unknown <philiprockert@gmail.com>
2024-01-15 11:51:18 -0500

    first commit

--- pages/monitoring.js
+++ pages/monitoring.js
@@ -107,8 +107,9 @@ const { data: session } = useSession();
       conection();
       
     try {
+      const status = 'pending'
       const response = await fetch(process.env.NEXT_PUBLIC_BASE_DB+'/obtener-mensajes');
-      const responseChats = await fetch(process.env.NEXT_PUBLIC_BASE_DB+'/obtener-chats');
+      const responseChats = await fetch(process.env.NEXT_PUBLIC_BASE_DB+`/consultar_por_status?status=${status}`);
       const responseUsers = await fetch(process.env.NEXT_PUBLIC_BASE_DB+'/obtener-usuarios');
       // El usuario está autenticado, puedes acceder a la sesión
       
@@ -117,8 +118,8 @@ const { data: session } = useSession();
       }
       const users = await responseUsers.json()
       const Id = iduser
-      const dataChats =  await responseChats.json();
-      const chatsPending = dataChats.filter(d=> d.status == 'pending')
+       
+      const chatsPending = await responseChats.json();
       const withoutGest = chatsPending.filter(d => d.userId == Id )
       console.log(Id)
       const data = await response.json();
