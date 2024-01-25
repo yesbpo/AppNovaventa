bca901a3beb58ea0b5d85d0d58c36f67eec8ef1c
unknown <philiprockert@gmail.com>
2024-01-25 17:47:39 -0500

    first commit

--- pages/chats.js
+++ pages/chats.js
@@ -992,7 +992,7 @@ const segundos = fechaActual.getSeconds().toString().padStart(2, '0');
 
   const fechaFinString = `${anioFin}-${mesFin}-${diaFin} ${horaFin}:${minutosFin}:${segundosFin}`;
 
-
+fetchMensajes()
   
     
   // Configurar el intervalo para realizar la consulta cada 30 segundos
