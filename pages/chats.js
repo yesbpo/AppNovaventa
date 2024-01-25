177d144cf3cb5f2a09525c0b35531b1a3bfd4458
unknown <philiprockert@gmail.com>
2024-01-24 19:49:48 -0500

    first commit

--- pages/chats.js
+++ pages/chats.js
@@ -1095,7 +1095,7 @@ const segundos = fechaActual.getSeconds().toString().padStart(2, '0');
       if (response.ok) {
         const data = await response.json();
         console.log(data);
-        
+        setMsg('')
           // Maneja la respuesta según tus necesidades
       } else {
         console.error('Error al realizar la solicitud:', response.status, response.statusText);
