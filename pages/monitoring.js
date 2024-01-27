095e4bf83fec84a5627682b93077137d17d10b2a
yesbpo <mesadeayuda@yesbpo.co>
2024-01-27 11:00:27 -0500

    diseñon

--- pages/monitoring.js
+++ pages/monitoring.js
@@ -708,7 +708,7 @@ setWebhookData(webhookText);
         })()}
       
         </Box>
-        <Box style={{ height: '30vw', width: '20vw'}}>
+        <Box style={{ height: '30vw', width: '10vw'}}>
         
           <div className="chat-container">
             <h1>{statuschats}</h1>
@@ -723,7 +723,8 @@ setWebhookData(webhookText);
             </ul>
           </div>
         </Box>
-        <Box>          <div className="chat-container">
+        <Box style={{ height: '30vw', width: '10vw'}}>
+          <div className="chat-container">
             <h1>En gestion</h1>
             <ul>
               {contactos2.map((contacto, index) => (
