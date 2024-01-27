43095d1f36bf80000acc4a2ea7c709887827e236
unknown <philiprockert@gmail.com>
2024-01-27 08:55:50 -0500

    first commit

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -58,7 +58,7 @@ const Sidebar = (props) => {
   const selectedItems =
   users.type_user === 'Coordinador' ? menuItems : 
   users.type_user === 'Asesor' ? menuItems1 :
-  defaultValue ? menuItems2 : null ; 
+  defaultValue ? menuItems2 : menuItems2;  
   // Routing.
   const router = useRouter();
   const activeMenu = useMemo(() => selectedItems.find((menu) => menu.link === router.pathname), [
