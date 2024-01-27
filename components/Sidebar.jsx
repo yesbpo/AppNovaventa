5d01acc7768cf7fe5ad600cd16b75c273b47b04a
unknown <philiprockert@gmail.com>
2024-01-27 09:04:14 -0500

    first commit

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -28,7 +28,9 @@ const menuItems = [
 const menuItems1 = [
   { id: 7, label: 'chats', icon: ChatIcon, link: '/chats' },
 ]
-
+const menuItems2 = [
+  { id: 7, label: 'cargando...', link: '/' },
+]
 const Sidebar = (props) => {
   const [toggleCollapse, setToggleCollapse] = useState(false);
   const [isCollapsible, setIsCollapsible] = useState(false);
@@ -53,9 +55,12 @@ const Sidebar = (props) => {
     fetchData();
   }, []);
   
-  const selectedItems =
-  users.type_user === 'Coordinador' ? menuItems : users.type_user === 'Asesor' ? menuItems1 : defaultValue;
+  const defaultValue = 'Cargando...';
 
+  const selectedItems =
+    users.type_user === 'Coordinador' ? menuItems : 
+    users.type_user === 'Asesor' ? menuItems1 :
+    users.type_user === undefined ? menuItems2 : defaultValue;
   // Routing.
   const router = useRouter();
   const activeMenu = useMemo(() => selectedItems.find((menu) => menu.link === router.pathname), [
