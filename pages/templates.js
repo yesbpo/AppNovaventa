7f0c34fccb6a35a6d5285342f7c74a9b4b6a3b86
SOPORTE 2023 <mesadeayuda@yesbpo.co>
2024-01-27 12:30:59 -0500

    gdfd

--- pages/templates.js
+++ pages/templates.js
@@ -509,7 +509,7 @@ const handleCreateTemplate = async () => {
               <input type="text" value={contentn} onChange={(e) => setContentn(e.target.value)} />
             </label>
             <br />
-            <Button type="submit" style={{fontWeight: 'bold',margin:'20px'}}>Agregar Contenido</Button>
+            <Button className="bg-grey" type="submit" style={{fontWeight: 'bold',margin:'20px'}}>Agregar Contenido</Button>
           </form>
           <p>{mensaje}</p>
         </div>
