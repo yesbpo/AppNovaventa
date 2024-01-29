48d74b9035021d59037649bd72b908bade29f602
yesbpo <mesadeayuda@yesbpo.co>
2024-01-29 11:19:01 -0500

    diseñon

--- pages/users.js
+++ pages/users.js
@@ -269,12 +269,14 @@ const CrearUsuario = () => {
   </BotonEnviar>
   </form>}
       {/* Lista de usuarios */}
-    <div styleclassName="max-h-screen overflow-y-auto">
+    <div style={{maxHeight: '100vh'}} className='overflow-y-auto'>
       <ul>
       {!showCrear && <h2 className="text-2xl font-bold text-center mb-3">Modificar Usuarios</h2> && usuarios.map((usuario) => (
       <li key={usuario.id} className='mb-2 flex items-center justify-between'>
         <span className='mr-2'>{usuario.complete_name}</span>
-        <button onClick={() => handleUsuarioSeleccionado(usuario)} className="bg-blue-500 text-white p-1 rounded-md">Modificar</button>
+        <button 
+          onClick={() => handleUsuarioSeleccionado(usuario)} 
+          className="bg-blue-500 text-white p-1 rounded-md ml-auto">Modificar</button>
       </li>
     ))}
   </ul>
