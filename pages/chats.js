20f36e880cdf41cc3023d909a7e545febc86a962
unknown <philiprockert@gmail.com>
2024-02-06 15:30:05 -0500

    cambios en permisos front

--- pages/chats.js
+++ pages/chats.js
@@ -1243,7 +1243,7 @@ fetchMensajes()
              {/* Mostrar Activos si 'mostrarActivos' es true */}
 
             {user.type_user === 'Asesor1' && <CustomButton onClick={openPopup}>Agregar Número</CustomButton>}
-            {user.type_user === 'Coordinador' && <CustomButton onClick={openPopup}>Agregar Número</CustomButton>}
+           
           </ButtonContainer>
         </Box>
         <Container>
