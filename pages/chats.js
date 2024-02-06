6b54fb26bce05a93b68bf2ec17148be409a4904e
unknown <philiprockert@gmail.com>
2024-02-06 16:06:19 -0500

    cambios en permisos front

--- pages/chats.js
+++ pages/chats.js
@@ -659,6 +659,7 @@ const fechaFinString = `${anioFin}-${mesFin}-${diaFin} ${horaFin}:${minutosFin}:
       const users = await responseUsers.json()
       setSession1(session.user.name)
       const Id = users.filter(d => d.usuario == session.user.name)
+      console.log(Id)
       const responseChatsin = await fetch(process.env.NEXT_PUBLIC_BASE_DB+`/consultar-chats/${Id[0].id}`);
       const chatsPending = await responseChatsin.json();
       
