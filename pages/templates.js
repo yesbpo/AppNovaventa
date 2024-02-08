cc23e4c2426145a995a785bbbff9a8d5b4ad15e9
SOPORTE 2023 <mesadeayuda@yesbpo.co>
2024-02-08 16:19:05 -0500

    maycol tenplates

--- pages/templates.js
+++ pages/templates.js
@@ -197,7 +197,7 @@ const handleCreateTemplate = async () => {
         'Content-Type': 'application/json',
         // Include any additional headers here
       },
-      body: JSON.stringify({ templateData: elementName }),
+      body: JSON.stringify({ elementName }),
     });
 
     const responseData = await response.json();
