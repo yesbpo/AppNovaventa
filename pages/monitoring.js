b5e864ca8091e6514448e358d2b5ad99c01dd62f
unknown <philiprockert@gmail.com>
2024-02-10 11:23:51 -0500

    ruta para consultar chats del dia actual en monitoreo

--- pages/monitoring.js
+++ pages/monitoring.js
@@ -185,7 +185,7 @@ useEffect(() => {
       }
       const data = await response.json();
       const chats = await responseChats.json();
-      console.log(chats)
+      console.log(Object.values(chats).map((chat) => chat.userId))
       const asesores = data.filter((d) => d.type_user === 'Asesor' ||  d.type_user === 'Asesor1' );
       setAsesores(asesores);
       console.log(asesores)
