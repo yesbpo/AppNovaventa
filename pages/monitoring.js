348a927e03110d0ee1757b128840d9d63ca6d677
unknown <philiprockert@gmail.com>
2024-02-13 13:18:14 -0500

    select para consultar chats por rango de fechas

--- pages/monitoring.js
+++ pages/monitoring.js
@@ -769,7 +769,7 @@ setWebhookData(webhookText);
           onClick={()=>{handleClosedClick()}}>
              {}Chats cerrados: {resultados2}
           </CustomButton>
-          <h1>tiempo de respuesta: {Object.values(Object.values(timeResponse)[0][0])}</h1>
+          {Object.values(Object.values(timeResponse)[0][0]).length > 0 && <h1>tiempo de respuesta: {Object.values(Object.values(timeResponse)[0][0])[0]}</h1>}
     </div>
     </ButtonContainer>
       </Box>
